3259cf2a3fa982b0956d56c8d447d93599cfd483
Mikhail <mihaspb2007@gmail.com>
2025-02-18 13:26:23 +0300

    Add 2 Homework

--- 2 Homework Strict and dynamic typing/main.py
+++ 2 Homework Strict and dynamic typing/main.py
@@ -1,1 +1,23 @@
-1
+# Strict typing
+a = 5
+b = "890"
+
+result = a + int(b)
+print(type(result))
+
+
+s = "abc"
+try:
+    result = int(s)
+except ValueError:
+    print("Error: ValueError невозможно преобразовать строку 'abc' в целое число.")
+
+
+# Dynamic typing
+num = 42
+print(type(num))
+
+num = "String"
+print(type(num))
+
+
