7b39c474c70c0a3e1d64831b68564f1616d1205a
Mikhail <mihaspb2007@gmail.com>
2025-02-18 18:45:10 +0300

    Adding spaces before and after operators

--- 4 Homework Variables/main.py
+++ 4 Homework Variables/main.py
@@ -1,10 +1,10 @@
 my_name, my_age, my_favorite_num = "Mikhail", 38, 3.14159
 
-print(my_name)
-print(my_age)
-print(my_favorite_num)
+print( my_name )
+print( my_age )
+print( my_favorite_num )
 
 my_age = my_age + 1
 my_favorite_num = 42
 
-print(f"My age: {my_age}, my favorite num: {my_favorite_num}")
+print(f"My age: { my_age }, my favorite num: { my_favorite_num }")
--- 6 Homework Exception handling/main.py
+++ 6 Homework Exception handling/main.py
@@ -1,12 +1,12 @@
 try:
-    num1 = float(input("Введите первое число: "))
-    num2 = float(input("Введите второе число: "))
-    result = num1/num2
+    num1 = float ( input("Введите первое число: "))
+    num2 = float ( input("Введите второе число: "))
+    result = num1 / num2
 except ValueError:
     print("Ошибка! Пожалуйста, введите числовое занчение!")
 except ZeroDivisionError:
-    print("Ошибка: Деление на ноль!")
+    print ("Ошибка: Деление на ноль!")
 else:
-    print("Результат операции деления: {result}")
+    print("Результат операции деления: { result }")
 finally:
     print("Программа деления завершена")
