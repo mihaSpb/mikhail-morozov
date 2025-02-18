2fd40a1163e233edce36f31cef9e3a4b55d83bdb
Mikhail <mihaspb2007@gmail.com>
2025-02-18 17:40:10 +0300

    Add 6 Homework

---
+++ 6 Homework Exception handling/main.py
@@ -0,0 +1,12 @@
+try:
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+    result = num1/num2
+except ValueError:
+    print("Ошибка! Пожалуйста, введите числовое занчение!")
+except ZeroDivisionError:
+    print("Ошибка: Деление на ноль!")
+else:
+    print("Результат операции деления: {result}")
+finally:
+    print("Программа деления завершена")
