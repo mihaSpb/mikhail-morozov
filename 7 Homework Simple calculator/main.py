6566412775d63ce2362675aa42c06a1d5d3e4a8b
Mikhail <mihaspb2007@gmail.com>
2025-02-19 19:15:14 +0300

    Add 7 fixed homework

--- 7 Homework Simple calculator/main.py
+++ 7 Homework Simple calculator/main.py
@@ -1,52 +1,37 @@
-# Сложение
+print("Выберите операцию:")
 print("1. Сложение")
-
-try:
-    num1 = float (input("Введите первое число: "))
-    num2 = float (input("Введите второе число: "))
-    result = num1 + num2
-except ValueError:
-    print("Ошибка! Пожалуйста, введите числовое занчение!")
-else:
-    print(f"Результат сложения: {result}\n")
-
-
-# Вычитание
 print("2. Вычитание")
-
-try:
-    num1 = float (input("Введите первое число: "))
-    num2 = float (input("Введите второе число: "))
-    result = num1 - num2
-except ValueError:
-    print("Ошибка! Пожалуйста, введите числовое занчение!")
-else:
-    print(f"Результат вычитания: {result}\n")
-
-
-# Умножение
 print("3. Умножение")
-
-try:
-    num1 = float ( input("Введите первое число: "))
-    num2 = float ( input("Введите второе число: "))
-    result = num1 * num2
-except ValueError:
-    print("Ошибка! Пожалуйста, введите числовое занчение!")
-else:
-    print(f"Результат умножения: {result}\n")
-
-
-# Деление
 print("4. Деление")
 
+
 try:
-    num1 = float ( input("Введите первое число: "))
-    num2 = float ( input("Введите второе число: "))
-    result = num1 / num2
+    operation = int (input("Введите номер операции (1/2/3/4): "))
+
+    if operation in (1,2,3,4):
+        num1 = float (input("Введите первое число: "))
+        num2 = float (input("Введите второе число: "))
+
+        if operation == 1:
+            result = num1 + num2
+            typeOperation = "сложения"
+        elif operation == 2:
+            result = num1 - num2
+            typeOperation = "вычитания"
+        elif operation == 3:
+            result = num1 * num2
+            typeOperation = "умножения"
+        elif operation == 4:
+            result = num1 / num2
+            typeOperation = "деления"
+    else:
+        print("Ошибка! введите корректный номер операции!")
+
 except ValueError:
     print("Ошибка! Пожалуйста, введите числовое занчение!")
+
 except ZeroDivisionError:
     print("Ошибка! Деление на ноль!")
+
 else:
-    print(f"Результат деления: {result}")
+    print(f"\nРезультат {typeOperation}: {result}")
