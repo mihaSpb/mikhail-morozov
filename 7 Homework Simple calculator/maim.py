7231ac8f3abd124299f0dae6cd84d01178f82ba4
Mikhail <mihaspb2007@gmail.com>
2025-02-19 12:14:31 +0300

    Add 7 homework

---
+++ 7 Homework Simple calculator/maim.py
@@ -0,0 +1,52 @@
+# Сложение
+print("1. Сложение")
+
+try:
+    num1 = float (input("Введите первое число: "))
+    num2 = float (input("Введите второе число: "))
+    result = num1 + num2
+except ValueError:
+    print("Ошибка! Пожалуйста, введите числовое занчение!")
+else:
+    print(f"Результат сложения: {result}")
+
+
+# Вычитание
+print("2. Вычитание")
+
+try:
+    num1 = float (input("Введите первое число: "))
+    num2 = float (input("Введите второе число: "))
+    result = num1 - num2
+except ValueError:
+    print("Ошибка! Пожалуйста, введите числовое занчение!")
+else:
+    print(f"Результат вычитания: {result}")
+
+
+# Умножение
+print("3. Умножение")
+
+try:
+    num1 = float ( input("Введите первое число: "))
+    num2 = float ( input("Введите второе число: "))
+    result = num1 * num2
+except ValueError:
+    print("Ошибка! Пожалуйста, введите числовое занчение!")
+else:
+    print(f"Результат умножения: {result}")
+
+
+# Деление
+print("4. Деление")
+
+try:
+    num1 = float ( input("Введите первое число: "))
+    num2 = float ( input("Введите второе число: "))
+    result = num1 / num2
+except ValueError:
+    print("Ошибка! Пожалуйста, введите числовое занчение!")
+except ZeroDivisionError:
+    print("Ошибка! Деление на ноль!")
+else:
+    print(f"Результат деления: {result}")
