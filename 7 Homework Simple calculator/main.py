3fa6f7ffe2e2475593ab5f0d857eb56bbd3351d5
Mikhail <mihaspb2007@gmail.com>
2025-02-19 23:33:18 +0300

    Add 7 fixed homework

--- 7 Homework Simple calculator/main.py
+++ 7 Homework Simple calculator/main.py
@@ -1,31 +1,28 @@
-print("Выберите операцию:")
-print("1. Сложение")
-print("2. Вычитание")
-print("3. Умножение")
-print("4. Деление")
-
 
 try:
+    print("Выберите операцию:")
+    print("1. Сложение")
+    print("2. Вычитание")
+    print("3. Умножение")
+    print("4. Деление")
+
     operation = int (input("Введите номер операции (1/2/3/4): "))
 
-    if operation in (1,2,3,4):
-        num1 = float (input("Введите первое число: "))
-        num2 = float (input("Введите второе число: "))
+    num1 = float (input("Введите первое число: "))
+    num2 = float (input("Введите второе число: "))
 
-        if operation == 1:
-            result = num1 + num2
-            typeOperation = "сложения"
-        elif operation == 2:
-            result = num1 - num2
-            typeOperation = "вычитания"
-        elif operation == 3:
-            result = num1 * num2
-            typeOperation = "умножения"
-        elif operation == 4:
-            result = num1 / num2
-            typeOperation = "деления"
-    else:
-        print("Ошибка! введите корректный номер операции!")
+    if operation == 1:
+        result = num1 + num2
+        typeOperation = "сложения"
+    elif operation == 2:
+        result = num1 - num2
+        typeOperation = "вычитания"
+    elif operation == 3:
+        result = num1 * num2
+        typeOperation = "умножения"
+    elif operation == 4:
+        result = num1 / num2
+        typeOperation = "деления"
 
 except ValueError:
     print("Ошибка! Пожалуйста, введите числовое занчение!")
