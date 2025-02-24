3bddf01ac2c023f74a047eb5eca16d1789d5e02a
Mikhail <mihaspb2007@gmail.com>
2025-02-24 18:43:07 +0300

    Upgrade 14 homework

--- 14 Homework Functions announcement and call/main.py
+++ 14 Homework Functions announcement and call/main.py
@@ -1,9 +1,10 @@
 def say_hello():
     print("Hello, Python!")
 
-def repeat_message(message):
-    print(message, message, message, sep="\n")
+def repeat_message(message, count):
+    result = (message + "\n") * count
+    print(result, end= "")
 
 
 say_hello()
-repeat_message("Сообщение для повтора")
+repeat_message("Сообщение для повтора", 3)
