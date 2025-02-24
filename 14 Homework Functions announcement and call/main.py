669ec1dc417b9d0ac356082dd0e41b0cf1544ece
Mikhail <mihaspb2007@gmail.com>
2025-02-24 17:13:02 +0300

    Add 10 homework

---
+++ 14 Homework Functions announcement and call/main.py
@@ -0,0 +1,9 @@
+def say_hello():
+    print("Hello, Python!")
+
+def repeat_message(message):
+    print(message, message, message, sep="\n")
+
+
+say_hello()
+repeat_message("Сообщение для повтора")
