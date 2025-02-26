361fe8b0878faabb22c4bb6be698bda5009ad3a6
Mikhail <mihaspb2007@gmail.com>
2025-02-26 18:06:30 +0300

    Upgrade 16 homework

---
+++ 16 Homework Built-in features/main.py
@@ -0,0 +1,6 @@
+my_string = "Hello, Python!"
+my_list = [1,2,3,4,5,6]
+
+print(f"Строка: {my_string};\nДлина строки {len(my_string)};\nТип объекта: {type(my_string)}\n")
+
+print(f"Строка: {my_list};\nДлина строки {len(my_list)};\nТип объекта: {type(my_list)}")
