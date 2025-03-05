f83b23b57fdd5dbc9100db5285dd7297315a30f8
Mikhail <mihaspb2007@gmail.com>
2025-03-05 12:45:33 +0300

    Add 24 homework

--- 24 Homework Replace list items/main.py
+++ 24 Homework Replace list items/main.py
@@ -1,7 +1,8 @@
+
+def replays_list_items (in_list):
+    if len(in_list) >= 2:
+        in_list[0], in_list[-1] = in_list[-1], in_list[0]
+        print(in_list)
+
 my_list = ["much", "loves", "bananas", "very", "Bob"]
-
-def replays_list_items (my_list):
-    my_list[0], my_list[-1] = my_list[-1], my_list[0]
-    return my_list
-
-print(replays_list_items(my_list))
+replays_list_items(my_list)
