b88e490e3e3bd48f89cddceb382cf90a2a13fe91
Mikhail <mihaspb2007@gmail.com>
2025-03-07 18:22:43 +0300

    Update 26 homework

--- 26 Homework Big practical task/main.py
+++ 26 Homework Big practical task/main.py
@@ -2,10 +2,6 @@
 def calculate_average(grades):
     return sum(grades) / len(grades) if grades else 0
 
-# Получение среднего балла студента
-def get_student_average(student):
-    return calculate_average(student["grades"])
-
 # Расчёт среднего балла всех студентов
 def calculate_average_all(students):
     total_sum = 0
@@ -23,6 +19,28 @@ def student_info(student):
     print(f"Средний балл: {average:.2f}")
     print(f"Статус: {status}\n")
 
+# Вывод на экран информации о студентах
+def print_all_students(students):
+    for student in students:
+        student_info(student)
+
+# Добавление нового студента
+def add_student(students, new_student):
+    students.append(new_student)
+
+# Вывод обновленной информацию о студентах
+def print_updated_students_info(students):
+    print("После добавления нового студента:")
+    print_all_students(students)
+    average_all = calculate_average_all(students)
+    print(f"Общий средний балл по всем студентам: {average_all:.2f}\n")
+
+# Удаление студента
+def remove_student_with_lowest_average(students):
+    student_to_remove = min(students, key=lambda s: calculate_average(s["grades"]))
+    students.remove(student_to_remove)
+    return student_to_remove
+
 
 def main():
     students = [
@@ -34,33 +52,25 @@ def main():
     ]
 
     print("Информация по каждому студенту:")
-    for student in students:
-        student_info(student)
+    print_all_students(students)
 
     avgerage_all = calculate_average_all(students)
     print(f"Общий средний балл по всем студентам: {avgerage_all:.2f}\n")
 
     # Добавление нового студента
     new_student = {"name": "Jon", "grades": [70, 80, 75]}
-    students.append(new_student)
-    print("После добавления нового студента:")
-    for student in students:
-        student_info(student)
-    avgerage_all = calculate_average_all(students)
-    print(f"Общий средний балл по всем студентам: {avgerage_all:.2f}\n")
+    add_student(students, new_student)
+    print_updated_students_info(students)
 
     # Удаление студента с самым низким средним баллом
-    student_to_remove = min(students, key=get_student_average)
-    students.remove(student_to_remove)
-    print(f"Удален студент с наименьшим средним баллом: {student_to_remove['name']}\n")
+    removed_student = remove_student_with_lowest_average(students)
+    print(f"Удален студент с наименьшим средним баллом: {removed_student['name']}\n")
 
     # Пересчет и вывод общего среднего балла после обновления списка студентов
     print("После удаления студента:")
-    for student in students:
-        student_info(student)
-    avgerage_all = calculate_average_all(students)
-    print(f"Общий средний балл по всем студентам: {avgerage_all:.2f}")
-
+    print_all_students(students)
+    average_all = calculate_average_all(students)
+    print(f"Общий средний балл по всем студентам: {average_all:.2f}")
 
 
 main()
