1da01084c25b1d1bb27a4522f8a510cc27f16241
Mikhail <mihaspb2007@gmail.com>
2025-03-10 15:46:39 +0300

    Add def 'add_book' in 28 homework

--- 28 Homework Cretion of a library/main.py
+++ 28 Homework Cretion of a library/main.py
@@ -7,13 +7,30 @@ def book_list_view(library):
         print("В библиотеке нет книг.")
 
 
+def add_book(library, title, author, year):
+    new_book = {"автор": author, "год": year, "наличие": None}
+
+    if title in library:
+        answer = input(f"\nКнига '{title}' уже существует. Обновить информацию о ней? (Да/Нет): ").strip().lower()
+        if answer == "да":
+            library[title] = new_book
+            print(f"Информация о книге '{title}' успешно обновлена.\n")
+        else:
+            print(f"Информация о книге '{title}' не обновлена.\n")
+    else:
+        library[title] = new_book
+        print(f"Книга '{title}' успешно добавлена.\n")
+
+
 def main ():
     library = {
         "Война и мир": {"автор": "Лев Толстой", "год": 1869, "наличие": "в наличии"},
         "Улисс": {"автор": "Джеймс Джойс", "год": 1921, "наличие": "выдана"},
-        "Колыбель для кошки": {"автор": "Курт Воннегут", "год": 1963, "наличие": "в наличии"}
+        "Колыбель для кошки": {"автор": "Курт Воннегут", "год": 1963}
     }
 
+    book_list_view(library)
+    add_book(library, "Улисс", "Джеймс Джойс", 1921)
     book_list_view(library)
 
 
